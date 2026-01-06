013e5cc8c196ea3a3275aded16cd3545309db95d
shravan jain <shravanjain3065@gmail.com>
2026-01-06 18:49:50 +0530

    implement path parameter

--- api_app.py
+++ api_app.py
@@ -2,6 +2,16 @@ from fastapi import FastAPI
 
 app=FastAPI()
 items=[{"name":"shravan"},{"age":21}]
-@app.get("/items/")
-def item():
-    return items[0:2]
+notes={
+       1:{"title":"today's journal","content":"An overview of what i done today"},
+    }
+
+# health endpoint
+@app.get("/health",status_code=200)
+def health_check():
+    return {"status":"running"}
+
+# path parameter for listing the note title
+@app.get("/notes/{id}")
+def item(id:int):
+    return notes[id]
